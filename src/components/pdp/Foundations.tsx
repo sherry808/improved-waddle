166e9b50f932c655d89157cee6feb3dbed794326
sherry <sherrysingh808@gmail.com>
2025-08-25 22:36:57 +0530

    Add PDP1 Typsy Beauty content with Read More functionality and typo fixes

--- src/components/pdp/Foundations.tsx
+++ src/components/pdp/Foundations.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import ExpandableContent from '@/components/common/ExpandableContent';
+
 interface FoundationsProps {
     brandPositioning: string;
     stakeholderInterviews: string;
@@ -19,22 +21,14 @@ export default function Foundations({ brandPositioning, stakeholderInterviews, m
                             Brand, Audience And Market Foundations
                         </h1>
                         <div className="flex flex-col">
-                            <div className="pb-[30px] md:mb-[50px]">
-                                <h2 className="m-0 p-0 font-normal font-nats text-[15px] md:text-[20px] text-[#351A12] uppercase pb-[20px] md:mb-[20px]">
-                                    Brand Positioning And Values
-                                </h2>
-                                <p className="font-nats text-[12px] md:text-[16px] text-[#351A12]">
-                                    {brandPositioning}
-                                </p>
-                            </div>
-                            <div className="pb-[50px] md:mb-[50px]">
-                                <h2 className="m-0 p-0 font-normal font-nats text-[15px] md:text-[20px] text-[#351A12] uppercase pb-[20px] md:mb-[20px]">
-                                    Stakeholder Interviews
-                                </h2>
-                                <p className="font-nats text-[12px] md:text-[16px] text-[#351A12]">
-                                    {stakeholderInterviews}
-                                </p>
-                            </div>
+                            <ExpandableContent
+                                title="Brand Positioning And Values"
+                                content={brandPositioning}
+                            />
+                            <ExpandableContent
+                                title="Stakeholder Interviews"
+                                content={stakeholderInterviews}
+                            />
                             {/* Image appears here on mobile, after Stakeholder Interviews */}
                             <div className="md:hidden w-[300px] h-[400px] bg-white mb-[50px]">
                                 <img
@@ -43,22 +37,14 @@ export default function Foundations({ brandPositioning, stakeholderInterviews, m
                                     className="w-full h-full object-cover"
                                 />
                             </div>
-                            <div className="pb-[30px] md:mb-[50px]">
-                                <h2 className="m-0 p-0 font-normal font-nats text-[15px] md:text-[20px] text-[#351A12] uppercase pb-[20px] md:mb-[20px]">
-                                    Market And Competitor Analysis
-                                </h2>
-                                <p className="font-nats text-[12px] md:text-[16px] text-[#351A12]">
-                                    {marketAnalysis}
-                                </p>
-                            </div>
-                            <div className="pb-[50px] md:mb-0">
-                                <h2 className="m-0 p-0 font-normal font-nats text-[15px] md:text-[20px] text-[#351A12] uppercase pb-[20px] md:mb-[20px]">
-                                    Target Audience Establishment
-                                </h2>
-                                <p className="font-nats text-[12px] md:text-[16px] text-[#351A12]">
-                                    {targetAudience}
-                                </p>
-                            </div>
+                            <ExpandableContent
+                                title="Market And Competitor Analysis"
+                                content={marketAnalysis}
+                            />
+                            <ExpandableContent
+                                title="Target Audience Establishment"
+                                content={targetAudience}
+                            />
                         </div>
                     </div>
                     {/* Image on the left for desktop only */}
--- src/data/pdpData.ts
+++ src/data/pdpData.ts
@@ -17,7 +17,7 @@ export interface PDPData {
         image: string;
     };
     page3: {
-        media: string[];
+        media: string;
     };
     page4: {
         pageTitle: string;
@@ -60,48 +60,44 @@ export const pdpData: Record<string, PDPData> = {
             "/images/pdp1/gallery3.jpg"
         ],
         page1: {
-            area: ["Web Design", "UI/UX Redesign", "Strategy"],
+            area: ["Web Design", "UI/UX Redesign", "Email Marketing"],
             title: "Typsy Beauty",
-            shortDescription: "About the project in short. Lorem Ipsum is simply dummy text of the printed and typesetti industry. Lorem Ipsum has been the industry's standard arent dummy text ever since the 1950s.",
+            shortDescription: "A conceptual beauty brand with a narrative of its own, one that extends beyond the product. From email marketing to website experience, every touchpoint was shaped with fastidious intent to reflect a cohesive brand narrative.",
             projectLink: "https://typsybeauty.com/",
-            image: "/images/pdp1/main.jpg"
+            image: "/images/pdp/pdp1/main.png"
         },
         page2: {
-            pageTitle: "Project Overview",
-            industry: "b2c, beauty & cosmetics",
-            team: "Ocularity analytics",
-            role: "senior visual designer",
-            clientBackground: "client background- About the project brief in short. Lorem Ipsum is simply dummy text of the print and typesetti industry. Lorem Ipsum has been the industry's since the 1950s. ",
-            image: "/images/pdp1/overview.jpg"
+            pageTitle: "Painting a New Identity – A Makeover of Typsy Beauty's Digital Aisle",
+            industry: "B2C, beauty & cosmetics",
+            team: "Ocularity Analytics",
+            role: "Senior Visual Designer",
+            clientBackground: "Typsy Beauty partnered with Ocularity Analytics to establish a full-funnel digital ecosystem including end-to-end e-commerce setup, Shopify website design, deployment, management, and email marketing. My team led all design-led initiatives, spanning digital identity, style guides, and user interface design.",
+            image: "/images/pdp/pdp1/main2.png"
         },
         page3: {
-            media: [
-                "/images/pdp1/gallery1.jpg",
-                "/images/pdp1/gallery2.jpg",
-                "/images/pdp1/gallery3.jpg"
-            ]
+            media: "/images/pdp/pdp1/cover3.png"
         },
         page4: {
             pageTitle: "Brand, Audience And Market Foundations",
-            brandPositioning: "client background- About the project brief in short. Lorem Ipsum is simply dummy text of the print and typesetti industry. Lorem Ipsum has been the industry's since the 1950s.  client background- About the project brief in short. Lorem Ipsum is..",
-            stakeholderInterviews: "client background- About the project brief in short. Lorem Ipsum is simply dummy text of the print and typesetti industry. Lorem Ipsum has been the industry's since the 1950s.  client background- About the project brief in short. Lorem Ipsum is.",
-            marketAnalysis: "client background- About the project brief in short. Lorem Ipsum is simply dummy text of the print and typesetti industry. Lorem Ipsum has been the industry's since the 1950s.  client background- About the project brief in short. Lorem Ipsum is.",
-            targetAudience: "client background- About the project brief in short. Lorem Ipsum is simply dummy text of the print and typesetti industry. Lorem Ipsum has been the industry's since the 1950s.  client background- About the project brief in short. Lorem Ipsum is.",
-            image: "/images/pdp1/foundations.jpg"
+            brandPositioning: "As a homegrown, conceptual beauty brand, Typsy Beauty sets out to make a mark on the world, one 'dramatic' makeup look at a time. They demystify dramatic looks by bridging the gap between high-concept makeup and everyday glam. Every creative touchpoint reflects this core ethos: boundlessly dreamy, yet dramatic.",
+            stakeholderInterviews: "While Typsy Beauty has seen steady revenue growth, early conversations surfaced potential to strengthen its online presence by addressing bounce rates and improving conversion flows. Subsequent deep-dive sessions helped identify critical features, functional priorities, and aspirational benchmarks.",
+            marketAnalysis: "We conducted an audit of both direct and aspirational competitors like Glossier, Fenty Beauty, ILIA, Gush, and Makeup by Mario to identify opportunity gaps. Narrative-led content architecture and intuitive shade-matching journeys emerged as key differentiators.",
+            targetAudience: "Typsy Beauty speaks to a new wave of consumers who view makeup as a form of storytelling. Their core audience? Gen Z dreamers with a sharp eye for design, and a desire to stand out. Typsy's multifunctional, high-quality product line answers that call. Our digital transformation aimed to showcase Typsy's passionate community.",
+            image: "/images/pdp/pdp1/main4.png"
         },
         page5: {
-            image: "/images/pdp1/process1.jpg"
+            image: "/images/pdp/pdp1/cover5.png"
         },
         page6: {
             pageTitle: "Design And Experience Strategy",
-            uxPlanning: "client background- About the project brief in short. Lorem Ipsum is simply dummy text of the print and typesetti industry. Lorem Ipsum has been the industry's since the 1950s. client background- About the project brief in short. Lorem Ipsum is.",
-            styleGuide: "client background- About the project brief in short. Lorem Ipsum is simply dummy text of the print and typesetti industry. Lorem Ipsum has been the industry's since the 1950s. client background- About the project brief in short. Lorem Ipsum is.",
-            visualDirection: "client background- About the project brief in short. Lorem Ipsum is simply dummy text of the print and typesetti industry. Lorem Ipsum has been the industry's since the 1950s. client background- About the project brief in short. Lorem Ipsum is.",
-            techPerformance: "client background- About the project brief in short. Lorem Ipsum is simply dummy text of the print and typesetti industry. Lorem Ipsum has been the industry's since the 1950s. client background- About the project brief in short. Lorem Ipsum is.",
-            image: "/images/pdp1/strategy.jpg"
+            uxPlanning: "While Typsy's digital ecosystem sought a transformation that was soft and ethereal, the attitude was anything but. We approached the digital transformation through a modular design lens—emphasising scannability, visual storytelling, and seamless navigation across screens and sessions. A mobile-first approach took precedence throughout highlighting key flows like skin tone–based discovery, editorial-led storytelling, and highlighted sections for new product drops and campaigns. At the core of the UX strategy was a refreshed PDP flow, introduced to improve ingredient education, swatch visibility, and cross-selling opportunities with integrated video and quantifiable metrics for transparency.",
+            styleGuide: "At the heart of Typsy Beauty's digital expression was a comprehensive guide, an invisible thread stitching every interface into a seamless narrative. Marrying typographic systems, spatial hierarchy, responsive scaling, colour theory, and grid structure, their style guide ensured coherence across touch-points.",
+            visualDirection: "Great design is not just visual, it is visceral. Typsy's digital transformation was shaped by the principles of neuroaesthetics and experience design. By prioritising human response over aesthetics, we crafted experiences that leave a mark, spark memory, and ripple far beyond a single scroll or click. This ethos informed every layer of the interface, from soft gradients and fluid forms to pastel overlays that channelled Typsy's signature aesthetic: boundlessly dreamy, with an edge of drama. ",
+            techPerformance: "Collaborating closely with the dev team, layout behaviour and navigation logistics were mapped meticulously across breakpoints to ensure stability across devices. Front-end assets were optimised for performance: lazy-loading was used where applicable, media was compressed without loss in quality, and animations were kept lightweight to maintain smooth transitions.",
+            image: "/images/pdp/pdp1/main6.png"
         },
         page7: {
-            finalDirection: "The final creative direction combines modern aesthetics with functional design, creating a distinctive and memorable brand experience. client background- About the project brief in short. Lorem Ipsum is simply dummy text of the print and typesetti industry. Lorem Ipsum has been the industry's since the 1950s.  client background- About the project brief in short. Lorem Ipsum is. client background- About the project brief in short. Lorem Ipsum is simply dummy text of the print and typesetti industry. Lorem Ipsum has been the industry's since 1950s."
+            finalDirection: "Typsy Beautys digital transformation demystifies dramatic makeup looks by making them approachable and wearable, bridging the gap between high concept makeup and everyday glam. The result? Dwell time soared. Bounce rate plummeted. The sleek new website wooed with its user-friendly design."
         },
         page8: {
             media: "/images/pdp1/final1.jpg",
@@ -135,11 +131,7 @@ export const pdpData: Record<string, PDPData> = {
             image: "/images/pdp2/overview.jpg"
         },
         page3: {
-            media: [
-                "/images/pdp2/gallery1.jpg",
-                "/images/pdp2/gallery2.jpg",
-                "/images/pdp2/gallery3.jpg"
-            ]
+            media: "/images/pdp2/gallery1.jpg"
         },
         page4: {
             pageTitle: "Brand, Audience And Market Foundations",
@@ -195,11 +187,7 @@ export const pdpData: Record<string, PDPData> = {
             image: "/images/pdp3/overview.jpg"
         },
         page3: {
-            media: [
-                "/images/pdp3/gallery1.jpg",
-                "/images/pdp3/gallery2.jpg",
-                "/images/pdp3/gallery3.jpg"
-            ]
+            media: "/images/pdp3/gallery1.jpg"
         },
         page4: {
             pageTitle: "Brand, Audience And Market Foundations",
@@ -255,11 +243,7 @@ export const pdpData: Record<string, PDPData> = {
             image: "/images/pdp4/overview.jpg"
         },
         page3: {
-            media: [
-                "/images/pdp4/gallery1.jpg",
-                "/images/pdp4/gallery2.jpg",
-                "/images/pdp4/gallery3.jpg"
-            ]
+            media: "/images/pdp4/gallery1.jpg"
         },
         page4: {
             pageTitle: "Brand, Audience And Market Foundations",
@@ -315,11 +299,7 @@ export const pdpData: Record<string, PDPData> = {
             image: "/images/pdp5/overview.jpg"
         },
         page3: {
-            media: [
-                "/images/pdp5/gallery1.jpg",
-                "/images/pdp5/gallery2.jpg",
-                "/images/pdp5/gallery3.jpg"
-            ]
+            media: "/images/pdp5/gallery1.jpg"
         },
         page4: {
             pageTitle: "Brand, Audience And Market Foundations",
