8f1739966fa179381bc8341eb9e5baa9404b07ca
Muskan1615 <vaishmuskan531@gmail.com>
2025-08-26 18:34:10 +0530

    update padding

--- src/components/pdp/DirectionAndExperience.tsx
+++ src/components/pdp/DirectionAndExperience.tsx
@@ -11,13 +11,13 @@ const DirectionAndExperience: React.FC<DirectionAndExperienceProps> = (
   props
 ) => {
   return (
-    <div className="flex flex-col lg:flex-row w-full bg-[#351A12] font-nats overflow-x-hidden p-10">
-      <div className="lg:w-1/2 w-full flex items-center justify-between p-8">
+    <div className="flex flex-col lg:flex-row w-full bg-[#351A12] font-nats overflow-x-hidden p-12">
+      <div className="lg:w-1/2 w-full flex items-center justify-start pl-8">
         <h2 className="m-0 font-monthis font-normal text-[54px] text-[#D2ADCE] leading-tight">
           {props.pageTitle}
         </h2>
       </div>
-      <div className="lg:w-1/2 w-full flex items-center justify-between pr-8">
+      <div className="lg:w-1/2 w-full flex items-center justify-center pr-8">
         <p className="font-nats text-[16px] text-[#F7F5ED]">
           {props.finalDirection}
         </p>
