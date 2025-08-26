466e9fa79e9bd5903317c493b5a4c223e21849c6
Muskan1615 <vaishmuskan531@gmail.com>
2025-08-26 20:16:30 +0530

    fix client mobile version

--- src/components/pdp/ClientInfo.tsx
+++ src/components/pdp/ClientInfo.tsx
@@ -11,39 +11,39 @@ interface ClientInfoProps {
 
 export default function ClientInfo(props: ClientInfoProps) {
   return (
-    <div className="flex flex-col lg:flex-row w-full font-nats overflow-hidden">
-      <div className="lg:w-[55%] w-full bg-[#351A12] flex items-center justify-center p-10">
-        <div className="w-full text-left p-10">
-          <h1 className="m-0 font-monthis font-normal text-[54px] text-[#D2ADCE] leading-tight mb-12">
+    <div className="flex flex-col-reverse lg:flex-row w-full font-nats overflow-hidden">
+      <div className="lg:w-[55%] w-full bg-[#351A12] flex items-center justify-center mobile:p-8 xl:p-10">
+        <div className="w-full text-left mobile:p-0 xl:p-10">
+          <h1 className="m-0 font-monthis font-normal text-[32px] lg:text-[54px] text-[#D2ADCE] leading-tight mobile:mb-4 mb-8 lg:mb-12">
             {props.pageTitle}
           </h1>
-          <div className="flex flex-col md:flex-row gap-16 mb-12">
+          <div className="flex flex-col md:flex-row flex-wrap gap-y-4 md:gap-x-8 lg:gap-x-16 mobile:mb-4 mb-8 lg:mb-12">
             <div className="flex-1">
-              <h2 className="m-0 font-nats font-normal text-[20px] text-[#F7F5ED] uppercase mb-2">
+              <h2 className="m-0 font-nats font-normal text-[15px] lg:text-[20px] text-[#F7F5ED] uppercase mb-2">
                 Industry
               </h2>
-              <p className="font-nats font-normal text-[16px] text-[#F7F5ED]">
+              <p className="font-nats font-normal text-[12px] lg:text-[16px] text-[#F7F5ED]">
                 {props.industry || "No industry information available."}
               </p>
             </div>
             <div className="flex-1">
-              <h2 className="m-0 font-nats font-normal text-[20px] text-[#F7F5ED] uppercase mb-2">
+              <h2 className="m-0 font-nats font-normal text-[15px] lg:text-[20px] text-[#F7F5ED] uppercase mb-2">
                 Team
               </h2>
-              <p className="font-nats font-normal text-[16px] text-[#F7F5ED]">
+              <p className="font-nats font-normal text-[12px] lg:text-[16px] text-[#F7F5ED]">
                 {props.team || "No team information available."}
               </p>
             </div>
             <div className="flex-1">
-              <h2 className="m-0 font-nats font-normal text-[20px] text-[#F7F5ED] uppercase mb-2">
+              <h2 className="m-0 font-nats font-normal text-[15px] lg:text-[20px] text-[#F7F5ED] uppercase mb-2">
                 Role
               </h2>
-              <p className="font-nats font-normal text-[16px] text-[#F7F5ED]">
+              <p className="font-nats font-normal text-[12px] lg:text-[16px] text-[#F7F5ED]">
                 {props.role || "No role information available."}
               </p>
             </div>
           </div>
-          <p className="font-nats text-[16px] leading-loose text-[#F7F5ED]">
+          <p className="font-nats text-[12px] lg:text-[16px] leading-loose text-[#F7F5ED]">
             {props.clientBackground ||
               "No client background information available."}
           </p>
