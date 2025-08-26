c1da056b94aa6a01d88c7ed53d029cf6bb4dc285
Muskan1615 <vaishmuskan531@gmail.com>
2025-08-26 18:30:14 +0530

    fix direction section of pdp1

--- src/components/pdp/DirectionAndExperience.tsx
+++ src/components/pdp/DirectionAndExperience.tsx
@@ -1,35 +1,29 @@
-'use client';
+"use client";
 
-import React from 'react';
+import React from "react";
 
 interface DirectionAndExperienceProps {
-    finalDirection: string;
+  pageTitle: string;
+  finalDirection: string;
 }
 
-const DirectionAndExperience: React.FC<DirectionAndExperienceProps> = ({
-    finalDirection
-}) => {
-    return (
-        <section className="bg-[#351A12] py-16 md:py-0">
-            <div className="px-0">
-                <div className="md:flex md:gap-[115px]">
-                    {/* Heading on the left for desktop */}
-                    <div className="flex flex-col px-[30px] md:px-[103px] md:pr-0 md:pt-[55px] md:pb-0 overflow-visible">
-                        <h2 className="m-0 p-0 font-normal text-[#D2ADCE] text-[32px] md:text-[54px] font-monthis pb-[35px] md:pb-[80px] leading-[47px] md:leading-[69px]">
-                            <span className="hidden md:inline">Final Creative Direction<br />& Web Experience</span>
-                            <span className="md:hidden">Final Creative Direction & Web Experience</span>
-                        </h2>
-                    </div>
-                    {/* Content on the right for desktop */}
-                    <div className="flex flex-col md:flex-1 px-[30px] md:px-[103px] md:pl-0 md:pt-[55px] md:pb-0">
-                        <p className="font-nats text-[12px] md:text-[16px] text-[#F7F5ED]">
-                            {finalDirection}
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+const DirectionAndExperience: React.FC<DirectionAndExperienceProps> = (
+  props
+) => {
+  return (
+    <div className="flex flex-col lg:flex-row w-full bg-[#351A12] font-nats overflow-x-hidden p-10">
+      <div className="lg:w-1/2 w-full flex items-center justify-between p-8">
+        <h2 className="m-0 font-monthis font-normal text-[54px] text-[#D2ADCE] leading-tight">
+          {props.pageTitle}
+        </h2>
+      </div>
+      <div className="lg:w-1/2 w-full flex items-center justify-between pr-8">
+        <p className="font-nats text-[16px] text-[#F7F5ED]">
+          {props.finalDirection}
+        </p>
+      </div>
+    </div>
+  );
 };
 
-export default DirectionAndExperience; 
+export default DirectionAndExperience;
