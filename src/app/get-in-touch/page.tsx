d27945b55de1d3536edea7e16922dcd592a1608b
Muskan1615 <vaishmuskan531@gmail.com>
2025-08-28 04:28:09 +0530

    fix get in touch screen

--- src/app/get-in-touch/page.tsx
+++ src/app/get-in-touch/page.tsx
@@ -1,26 +1,24 @@
-'use client';
+"use client";
 
-import React from 'react';
-import GetInTouch from '@/components/get-in-touch/GetInTouch';
-import SignupBanner from '@/components/landing/SignupBanner';
-import Logos from '@/components/landing/Logos';
+import React from "react";
+import GetInTouch from "@/components/get-in-touch/GetInTouch";
+import SignupBanner from "@/components/landing/SignupBanner";
+import Logos from "@/components/landing/Logos";
 // import ContactForm from '@/components/get-in-touch/ContactForm';
 // import ContactInfo from '@/components/get-in-touch/ContactInfo';
-import Footer from '@/components/landing/Footer';
+import Footer from "@/components/landing/Footer";
 
 export default function GetInTouchPage() {
-    return (
-        <main className="pt0">
-            <div className="w-full">
-                {/* <div className="max-w-[360px]"> */}
-                <GetInTouch />
-                <Logos />
-                {/* <ContactForm /> */}
-                {/* <ContactInfo /> */}
-                {/* </div> */}
-                <SignupBanner />
-                <Footer />
-            </div>
-        </main>
-    );
-} 
+  return (
+    <main className="pt-0 overflow-x-hidden">
+      {/* <div className="max-w-[360px]"> */}
+      <GetInTouch />
+      <Logos />
+      {/* <ContactForm /> */}
+      {/* <ContactInfo /> */}
+      {/* </div> */}
+      <SignupBanner />
+      <Footer />
+    </main>
+  );
+}
