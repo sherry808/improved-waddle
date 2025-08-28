e0355d53718c2f064884b67e95f6a7bc08f4c3ae
Muskan1615 <vaishmuskan531@gmail.com>
2025-08-29 04:24:32 +0530

    make meet the creator page

--- src/app/meet-creator/page.tsx
+++ src/app/meet-creator/page.tsx
@@ -1,9 +1,15 @@
-import Hero from '@/components/meet-creator/Hero';
+"use client";
+
+import Footer from "@/components/landing/Footer";
+import Logos from "@/components/landing/Logos";
+import MeetCreator from "@/components/landing/MeetCreator";
 
 export default function MeetCreatorPage() {
-    return (
-        <main>
-            <Hero />
-        </main>
-    );
-} 
+  return (
+    <main className="pt-0 overflow-x-hidden">
+      <MeetCreator />
+      <Logos />
+      <Footer />
+    </main>
+  );
+}
