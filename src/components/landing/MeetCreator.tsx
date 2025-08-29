7afb90508d8835604a70032ed06d0012bd814ad1
Muskan1615 <vaishmuskan531@gmail.com>
2025-08-29 20:25:50 +0530

    fix read more button

--- src/components/landing/MeetCreator.tsx
+++ src/components/landing/MeetCreator.tsx
@@ -36,14 +36,16 @@ const MeetCreator: React.FC = () => {
                 {paragraph}
               </p>
             ))}
-          <div className="font-nats text-[12px] leading-[33px] text-[#351A12] mx-auto mt-0 flex justify-center">
-            <button
-              onClick={() => setIsExpanded(!isExpanded)}
-              className="inline-block w-full text-center px-6 mt-4 font-nats font-normal text-[15px] lg:text-[20px] text-[#351A12] uppercase bg-[#D2ADCE]"
-            >
-              {isExpanded ? "Read Less" : "Read More"}
-            </button>
-          </div>
+          {!isExpanded && (
+            <div className="font-nats text-[12px] leading-[33px] text-[#351A12] mx-auto mt-0 flex justify-center">
+              <button
+                onClick={() => setIsExpanded(true)}
+                className="inline-block w-full text-center px-6 mt-4 font-nats font-normal text-[15px] lg:text-[20px] text-[#351A12] uppercase bg-[#D2ADCE]"
+              >
+                Read More
+              </button>
+            </div>
+          )}
         </div>
         <div className="hidden lg:block">
           {allParagraphs.map((paragraph, index) => (
@@ -60,7 +62,7 @@ const MeetCreator: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col lg:flex-row w-full font-nats overflow-hidden px-[30px] py-10 lg:p-[50px] xl:p-[70px] desktop-xl:p-[100px] bg-[#F0E5D4] justify-between">
+    <div className="flex flex-col lg:flex-row w-full font-nats overflow-hidden px-[30px] py-10 lg:p-[50px] xl:p-[70px] desktop-xl:p-[102px] bg-[#F0E5D4] justify-between">
       <div className="w-full lg:w-[40%] xl:w-[30%] h-auto text-left">
         <div className="flex flex-col">
           <h1 className="m-0 font-nats font-normal text-[15px] lg:text-[20px] mb-3 lg:mb-4 uppercase text-[#351A12]">
@@ -71,7 +73,7 @@ const MeetCreator: React.FC = () => {
           </h1>
         </div>
       </div>
-      <div className="w-full lg:w-[60%] xl:w-[70%] flex flex-col justify-start pl-0 lg:pl-7 desktop-xl:pl-4">
+      <div className="w-full lg:w-[60%] xl:w-[70%] flex flex-col justify-start pl-0 lg:pl-7 desktop-xl:pl-5">
         {renderContent()}
       </div>
     </div>
