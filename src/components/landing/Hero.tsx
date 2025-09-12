45d3f37d8d872330aec6822751dc9a637787961a
Muskan1615 <vaishmuskan531@gmail.com>
2025-09-12 18:14:36 +0530

    fix hero landing page

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -15,7 +15,7 @@ export default function Hero() {
   }
 
   return (
-    <div className="px-[38px] md:px-[103px] lg:px-[424px] pt-[168px] lg:pt-[104px] pb-[58px] lg:pb-[55px] h-[549px] lg:h-[693px] bg-[#F0E5D6]">
+    <div className="px-[38px] md:px-[103px] xl:px-[424px] pt-[168px] md:pt-[100px] xl:pt-[104px] pb-[58px] xl:pb-[55px] h-[549px] lg:h-[693px] bg-[#F0E5D6]">
       <BoxCarouselDemo />
     </div>
   );
