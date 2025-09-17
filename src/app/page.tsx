daeea44c9d3c87f3ed8741709856352e8a68e0d5
Muskan1615 <vaishmuskan531@gmail.com>
2025-09-17 17:49:05 +0530

    fix landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <Hero />
       <Launch />
       <ProjectDisplay />
-      {/* <Projects /> */}
+      <Projects />
       <MeetCreator />
       <Services />
       <Contact />
